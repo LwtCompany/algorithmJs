5f7a3ca26a456699a2fcd827d9575d08e164388f
LwtCompany <hunter982019@gmail.com>
2022-01-07 20:40:35 +0500

    added: max_candle_count

---
+++ js/max_candle_count.js
@@ -0,0 +1,10 @@
+function birthdayCakeCandles(candles) {
+    let max_value = Math.max(...candles), t = 0;
+    for(i = 0; i < candles.length; i++){
+        if(candles[i] == max_value)
+            t++;
+    }
+    return t;
+}
+
+console.log(birthdayCakeCandles([1,2,3,4,5,6,6, 7]))
